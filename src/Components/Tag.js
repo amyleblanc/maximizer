9ac84fd54c14f37f1c8ba3e7665c25ce3a6f6d90
amyleblanc <amymccarthy2014@gmail.com>
2022-06-02 00:28:56 -0700

    create component for task tags & avatar

---
+++ src/Components/Tag.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+import {
+  Avatar,
+  Box,
+  CardContent,
+  Typography,
+} from "@mui/material/";
+
+export default function Tag({task}) {
+
+  const getInitials = (string) => {
+    const initials = string
+      .split(" ")
+      .map((word) => word[0])
+      .join("");
+
+    return initials.toUpperCase();
+  };
+
+  return (
+    <>
+      <CardContent sx={{ paddingLeft: "8px" }}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "flex-start",
+            }}
+          >
+            {task.Tags.split(",").map((tag) => (
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  margin: 1,
+                  marginRight: 0,
+                  backgroundColor: "#ECEEEE",
+                  border: 1,
+                  borderColor: "#ECEEEE",
+                  borderRadius: "8px",
+                  padding: "4px",
+                }}
+              >
+                <Typography color="gray" variant="p">
+                  {tag}
+                </Typography>
+              </Box>
+            ))}
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignSelf: "center",
+            }}
+          >
+            <Avatar>{getInitials(task.Assignee)}</Avatar>
+          </Box>
+        </Box>
+      </CardContent>
+    </>
+  )
+}
